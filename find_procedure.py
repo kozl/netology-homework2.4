2b84050e3d4bb285d3c0ba17f9b645f67bc7eb29
Aleksandr Kozlov <avlkozlov@avito.ru>
2017-08-09 13:14:35 +0300

    Доработка

--- find_procedure.py
+++ find_procedure.py
@@ -7,32 +7,29 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 migrations = os.path.join(current_dir, 'Migrations')
 
 
-def grep(filename, string):
-    try:
-        with open(filename) as f:
-            for line in f:
-                if string in line:
-                    return True
-        return False
-    except:
-        print(filename)
-        raise
+def grep(filename, str):
+    with open(filename) as f:
+        text = f.read()
+        if str in text:
+            return True
+        else:
+            return False
 
 
-def get_filepath(path=migrations, match='.sql'):
+def get_filepaths(path=migrations, match='.sql'):
     filepaths = []
     for root, dirs, files in os.walk(path):
         for filename in files:
-            if match in filename:
+            if filename.endswith(match):
                 filepath = os.path.join(root, filename)
                 filepaths.append(filepath)
     return filepaths
 
 if __name__ == '__main__':
-    files = get_filepath()
+    files = get_filepaths()
     while True:
-        string = input('Введите строку: ')
-        files = [file for file in files if grep(file, string)]
+        str = input('Введите строку: ')
+        files = [file for file in files if grep(file, str)]
         for file in files:
             print(file)
         print('Всего: {}'.format(len(files)))
